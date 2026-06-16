bd761651b9a3374bd064d2ecb64c5534f3de5efa
Devendra Pratap Singh <devendraa_01@gmail.com>
2026-06-16 16:39:48 +0530

    Building corner db

---
+++ PatternDatabases/CornerDBMaker.h
@@ -0,0 +1,25 @@
+//
+// Created by Devendra Pratap Singh  on 16-06-2026.
+//
+
+#ifndef RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#define RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
+#include "CornerPatternDatabase.h"
+#include "../Model/RubikCubeBitboard.cpp"
+
+using namespace std;
+
+class CornerDBMaker {
+private:
+    string fileName;
+    CornerPatternDatabase cornerDB;
+
+public:
+    CornerDBMaker(string _fileName);
+    CornerDBMaker(string _fileName, uint8_t init_val);
+
+    bool bfsAndStore();
+};
+
+
+#endif //RUBIKS_CUBE_SOLVER_CORNERDBMAKER_H
